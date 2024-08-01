ae15b9a3b9029bef73f65ab19b71b0ebaa60deb6
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-08-01 14:44:32 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 汇总常用插件
+
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+    	echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+
+merge_package main https://github.com/kenzok8/small-package package/app luci-app-adguardhome
+merge_package v5 https://github.com/sbwml/luci-app-mosdns package/app luci-app-mosdns mosdns v2dat
+
+# 集客AC
+
+merge_package main https://github.com/lwb1978/openwrt-gecoosac package/app luci-app-gecoosac
+merge_package main https://github.com/lwb1978/openwrt-gecoosac package/app gecoosac
+
+echo 'src-git alist https://github.com/sbwml/luci-app-alist.git' >>feeds.conf.default
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.default
+git clone -b js --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/app/luci-app-unblockneteasemusic
+
+# Add a feed source
+# 添加额外软件包
+git clone --depth 1  https://github.com/ilxp/luci-app-ikoolproxy.git package/app/luci-app-ikoolproxy
+
+# 仿istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
+
+# 科学上网插件
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git ssrplus https://github.com/fw876/helloworld.git;master" >>feeds.conf.default
+# merge_package master https://github.com/fw876/helloworld package/app/ssrplus lua-neturl redsocks2 shadow-tls v2raya luci-app-ssr-plus
+
+# 开发版openclash
+merge_package dev https://github.com/vernesong/OpenClash package/app luci-app-openclash
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
+# 下载openclash内核
+mkdir -p package/app/luci-app-openclash/root/etc/openclash/core/
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz | tar xOvz > package/app/luci-app-openclash/root/etc/openclash/core/clash
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz | gunzip -c > package/app/luci-app-openclash/root/etc/openclash/core/clash_tun
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz | tar xOvz > package/app/luci-app-openclash/root/etc/openclash/core/clash_meta
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > package/app/luci-app-openclash/root/etc/openclash/GeoIP.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > package/app/luci-app-openclash/root/etc/openclash/GeoSite.dat
+chmod +x package/app/luci-app-openclash/root/etc/openclash/core/clash*
+
+./scripts/feeds update -a
+
+\rm -rf feeds/ssrplus/mosdns
+\rm -rf feeds/packages/net/mosdns
+# \rm -rf feeds/luci/applications/luci-app-mosdns feeds/packages/utils/v2dat
+\rm -rf feeds/luci/applications/luci-app-passwall
+# \rm -rf feeds/luci/applications/luci-app-ddns-go
+\rm -rf feeds/luci/applications/luci-app-alist
+\rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
+
+rm -rf feeds/luci/applications/luci-app-openclash
+
+merge_package master https://github.com/coolsnowwolf/luci feeds/luci/applications applications/luci-app-pppwn
+merge_package master https://github.com/coolsnowwolf/packages feeds/packages/multimedia multimedia/pppwn-cpp
+merge_package master https://github.com/coolsnowwolf/luci feeds/luci/applications applications/luci-app-easymesh
+
+# fix : ubus_call_umdns()=ubus.c@1385
+rm package/network/services/umdns/files/umdns.init
+rm package/network/services/umdns/files/umdns.json
+wget -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.init
+wget -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.json
+
+./scripts/feeds install -a
+
+# 修复feeds错误
+sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
+
+# 主题
+git clone https://github.com/thinktip/luci-theme-neobird.git feeds/luci/themes/luci-theme-neobird
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git feeds/luci/themes/luci-theme-opentomcat
+git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git feeds/luci/themes/luci-theme-kucat
+git clone https://github.com/derisamedia/luci-theme-alpha.git feeds/luci/themes/luci-theme-alpha
+
+# echo '### Argon Theme Config ###'
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b master  https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config # if have
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
---
+++ diy-part2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 修复编译错误
+# sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' package/app/cdnspeedtest/Makefile
+
+# 1.修改默认ip
+sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
+
+# 2.修改主机名
+sed -i 's/immortalwrt/MR3000D-CIq/g' package/base-files/files/bin/config_generate
+
+# 4.修改版本号
+
+
+# 5.修改默认主题
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+# 或
+# default_theme='alpha'
+# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i "s/luci-static\/bootstrap/luci-static\/argon/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 6.设置ttyd免登录
+# sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+
+# 7.修正连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+
+# 替换终端为bash
+# sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
+# 删除默认密码
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+
+# 修改wifi名字
+# sed -i 's/ssid='ImmortalWrt'/ssid='CMCC'/' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
